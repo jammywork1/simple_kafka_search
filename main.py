451450f2c85494772b6bdabd81a29f3ab9e616fd
r.nikiforov <r.nikiforov@tinkoff.ru>
2018-04-27 11:50:25 +0300

    catch deserialization errors and eval filter errors. add option parametr --show_warnings

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
 from optparse import OptionParser
@@ -34,26 +34,37 @@ def main(options):
             else:
                 print(msg.error())
                 return
-        message = _bytes2dict_deserialize(msg.value())
         offset = msg.offset()
-        # ts_type, timestamp_value = msg.timestamp()
-        # recieved = datetime.fromtimestamp(timestamp_value)
-        criteria = eval(options.filter)
-        if criteria == True:
-            # print('-> offset {0}, recieved {1:%d-%m-%Y %H:%M:%S}'.format(offset, recieved))
-            print('-> offset {0}'.format(offset))
-            pprint(message)
-            printed += 1
-            if options.number and printed >= options.number:
-                break
+        try:
+            message = _bytes2dict_deserialize(msg.value()) 
+        except json.decoder.JSONDecodeError:
+            if options.show_warnings:
+                print('-> offset {0} : Json Deserialize Error'.format(offset)) 
+        else:
+            # ts_type, timestamp_value = msg.timestamp()
+            # recieved = datetime.fromtimestamp(timestamp_value)
+            try:
+                criteria = eval(options.filter)
+            except (KeyError, TypeError) as e:
+                criteria = False
+                if options.show_warnings:
+                    print(f'-> offset {offset} : {e}')
+            if criteria == True:
+                # print('-> offset {0}, recieved {1:%d-%m-%Y %H:%M:%S}'.format(offset, recieved))
+                print('-> offset {0}'.format(offset))
+                pprint(message)
+                printed += 1
+                if options.number and printed >= options.number:
+                    break
         print('{0}'.format(offset), end='\r')
     c.close()
-    print('end')
+    print('end             ')
 
 parser = OptionParser()
 parser.add_option("-s", "--bootstrap_servers")
 parser.add_option("-t", "--topic")
 parser.add_option("-f", "--filter")
+parser.add_option("-w", "--show_warnings", default='f')
 parser.add_option("-n", "--number", type="int")
 
 (options, args) = parser.parse_args()
@@ -64,8 +75,12 @@ if not options.topic:
     parser.error("options topic cant be empty")
 if not options.filter:
     parser.error("options filter cant be empty")
-
-
+if options.show_warnings not in ('f', 't'):
+    parser.error("options show_warnings maybe only 't' or 'f'")
+if options.show_warnings == 'f':
+    options.show_warnings = False
+else:
+    options.show_warnings = True
 if __name__ == "__main__":
     main(options)
 
